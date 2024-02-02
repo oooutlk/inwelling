df4f5a186b27d2f92cc2010a6e9ac2e88990432b
oooutlk <oooutlk@outlook.com>
2024-02-02 19:38:07 +0800

    version 0.5.2
    1. Don't sleep, check potential paths and wait for their manifest.inwelling.

--- src/lib.rs
+++ src/lib.rs
@@ -48,13 +48,11 @@
 //!    ```
 
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     env,
     fs::{self, File},
     io::Write,
     path::{Path, PathBuf},
-    thread,
-    time,
 };
 
 /// Information collected from downstream crates.
@@ -138,9 +136,9 @@ impl Default for Opts {
 ///
 /// - Optional .rs file paths.
 pub fn collect_downstream( Opts{ watch_manifest, watch_rs_files, dump_rs_paths }: Opts ) -> Downstream {
-    let manifest_paths = locate_manifest_paths();
-
     let build_name = env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME");
+
+    let manifest_paths = locate_manifest_paths( &build_name );
 
     manifest_paths.into_iter().fold( Downstream::default(), |mut inwelling, (manifest_path, upstreams)| {
         if upstreams.contains( &build_name ) {
@@ -195,29 +193,92 @@ pub fn collect_downstream( Opts{ watch_manifest, watch_rs_files, dump_rs_paths }
 // the path of the file that stores the downstream crate's manifest directory.
 const MANIFEST_DIR_INWELLING: &'static str = "manifest_dir.inwelling";
 
-fn locate_manifest_paths() -> HashMap<PathBuf,Vec<String>> {
+fn watch_or_not_watch( build_name: &str, build_dir: &Path ) -> (HashSet<String>,HashSet<PathBuf> ) {
+    let mut watch = HashSet::<String>::new(); // Package name
+    let mut not_watch = HashSet::<PathBuf>::new(); // Package name + uniq id suffix
+
+    for entry in build_dir.read_dir().unwrap() {
+        if let Ok( entry ) = entry {
+            let path = entry.path();
+            if path.is_dir() {
+                for entry in path.read_dir().unwrap() {
+                    if let Ok( entry ) = entry {
+                        let path = entry.path();
+                        if path
+                            .file_stem()
+                            .and_then( |s| s.to_str() )
+                            .map( |s| s.starts_with( "build_script_build" ))
+                            .unwrap_or( false )
+                        {
+                            if let Some( ext ) = path.extension() {
+                                if ext == "d" {
+                                    for line in fs::read_to_string( dbg!(&path) ).unwrap().lines() {
+                                        if let Some(pos) = line.find(':') {
+                                            for s in line[pos+1..].split(' ') {
+                                                if s.ends_with(".rs") {
+                                                    let build_script = PathBuf::from( s );
+                                                    if let Ok( contents ) = fs::read_to_string( dbg!(&build_script) ) {
+                                                        if contents.contains("inwelling::to") &&
+                                                            contents.contains( &format!("\"{build_name}\""))
+                                                        {
+                                                            dbg!( &path, &build_script );
+                                                            let parent = path.parent().unwrap();
+                                                            let filename = parent
+                                                                .file_name().unwrap()
+                                                                .to_str()   .unwrap();
+                                                            let pos = filename.rfind('-').unwrap();
+                                                            watch.insert( filename[0..pos].to_owned() );
+                                                            not_watch.insert( parent.to_owned() );
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    (watch, not_watch)
+}
+
+fn locate_manifest_paths( build_name: &str ) -> HashMap<PathBuf,Vec<String>> {
     let mut path_bufs = HashMap::new();
 
     let out_dir = PathBuf::from( env::var( "OUT_DIR" ).expect( "$OUT_DIR should exist." ));
     let ancestors = out_dir.ancestors();
     let build_dir = ancestors.skip(2).next().expect( "'build' directory should exist." );
 
-    thread::sleep( time::Duration::from_secs(1) );
+    let (watch, not_watch) = watch_or_not_watch( build_name, &build_dir );
 
-    for entry in build_dir.read_dir().expect( &format!( "to list all sub dirs in {:?}", build_dir )) {
-        if let Ok( entry ) = entry {
-            let path = entry.path();
-            if path.is_dir() {
-                let inwelling_file_path = path.join("out").join( MANIFEST_DIR_INWELLING );
-                if inwelling_file_path.exists() {
-                    let contents = fs::read_to_string( &inwelling_file_path )
-                        .expect( &format!( "to read {:?} to get one manifest path", inwelling_file_path ));
-                    let mut lines = contents.lines();
-                    let manifest_dir = lines.next()
-                        .expect( &format!( "{:?} should contain the line of manifest dir.", inwelling_file_path ));
-                    path_bufs
-                        .entry( PathBuf::from( manifest_dir ).join( "Cargo.toml" ))
-                        .or_insert_with( || lines.map( ToOwned::to_owned ).collect() );
+    let mut pending = true;
+    while pending {
+        pending = false;
+        for entry in build_dir.read_dir().expect( &format!( "to list all sub dirs in {:?}", build_dir )) {
+            if let Ok( entry ) = entry {
+                let path = entry.path();
+                if path.is_dir() {
+                    let inwelling_file_path = path.join("out").join( MANIFEST_DIR_INWELLING );
+                    if inwelling_file_path.exists() {
+                        let contents = fs::read_to_string( &inwelling_file_path )
+                            .expect( &format!( "to read {:?} to get one manifest path", inwelling_file_path ));
+                        let mut lines = contents.lines();
+                        let manifest_dir = lines.next()
+                            .expect( &format!( "{:?} should contain the line of manifest dir.", inwelling_file_path ));
+                        path_bufs
+                            .entry( PathBuf::from( manifest_dir ).join( "Cargo.toml" ))
+                            .or_insert_with( || lines.map( ToOwned::to_owned ).collect() );
+                    } else if let Some(s) = path.to_str() {
+                        if let Some(pos) = s.rfind('-') {
+                            if watch.contains( &s[..pos] ) && !not_watch.contains( &path ) {
+                                pending = true;
+                            }
+                        }
+                    }
                 }
             }
         }
